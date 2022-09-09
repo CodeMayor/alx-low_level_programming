a593c3e27b5e5ea30f7d1efb5f3d877d5a55ebc0
Codemayor <musaabdulsalam21@gmail.com>
2022-09-08 18:27:27 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,7 @@
-#inclide <stdio.h>
-/**
- * main : print the string in the main function
- * description : using the main function
- * this programm prints ""Programming is like buil   ding a multilingual puzzle"
- * Return :0
- */
-int main (void)
+#include <stdio.h>
+
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+       	puts("Programming is like building a multilingual puzzle\n");
+       	return (0);
 }
